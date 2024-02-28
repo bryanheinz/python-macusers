2cfc68f86d44b9f1b8b58e27cc24464381658272
bryan heinz <git@bryanheinz.com>
2024-02-28 16:14:41 -0600

    updated version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="macusers",
-    version="0.1.0",
+    version="1.0.0",
     author="Bryan Heinz",
     author_email="pypi@bryanheinz.com",
     description="Get the macOS console username and/or a list of local non-system users.",
